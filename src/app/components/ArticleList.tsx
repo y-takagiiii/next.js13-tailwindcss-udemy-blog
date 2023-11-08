9e24e38f829dd562c23b7a438dcd14912695e884
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-08 16:59:19 +0900

    イメージurlを変更

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -14,7 +14,7 @@ const ArticleList: React.FC<Props> = ({ articles }) => {
         <article className='shadow my-4 flex flex-col' key={article.id}>
           <Link href={`/articles/${article.id}`} className='hover:opacity-75'>
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt='記事イメージ'
               width={1280}
               height={300}
