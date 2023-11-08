260e30302149ff635dc6fd80b92cdc69dc0e626d
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-08 15:45:10 +0900

    SSRを使うよう変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { getAllArticles } from "@/blogAPI";
 import ArticleList from "./components/ArticleList";
 
-export default function Home() {
+export default async function Home() {
+  const articles = await getAllArticles();
+  console.log(articles);
   return (
     <div className="md:flex">
       <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-        <ArticleList />
+        <ArticleList articles={articles}/>
       </section>
 
       <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
