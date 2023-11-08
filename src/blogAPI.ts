6fb4412de02565e0e653b07ee7c8ab3ba058ef6b
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-08 17:58:35 +0900

    記事新規作成APIを作成

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -28,3 +28,24 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json();
   return article;
 };
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDateTime = new Date().toISOString();
+  const res = await fetch(
+    'http://localhost:3002/posts',
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id, title, content, createdAt: currentDateTime })
+    }
+  );
+  if (!res.ok) {
+    throw new Error("記事を作成できませんでした。");
+  }
+  const newArticle = await res.json();
+  return newArticle;
+};
