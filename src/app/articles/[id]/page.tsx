9636685c9677e20c1b9c6178291bd6ac9df221f0
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-08 16:58:07 +0900

    ダイナミックルーティングを使った個別記事ページを作成

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { getDetailArticle } from '@/blogAPI';
+import Image from 'next/image';
+import React from 'react'
+
+const Article = async ({ params }: { params: { id: string } }) => {
+  const detailArticle = await getDetailArticle(params.id);
+  return (
+    <div className='max-w-3xl mx-auto p-5'>
+      <Image
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
+        alt='記事イメージ'
+        width={1280}
+        height={300}
+      />
+      <h1 className='text-4xl text-center mb-10 mt-10'>
+        {detailArticle.title}
+      </h1>
+      <div className='text-lg leading-relaxed text-justify'>
+        <p>{detailArticle.content}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Article;
