68dd5dec69c9e7a08c2aea054df27f61b9e1c798
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-08 10:32:19 +0900

    Footerコンポーネントを読み込み

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Header from './Header'
+import Footer from './Footer'
 
 export const metadata: Metadata = {
   title: 'Next.js13 Blog',
@@ -17,7 +18,7 @@ export default function RootLayout({
       <body className='container mx-auto bg-slate-700 text-slate-50'>
         <Header/>
         {children}
-        <footer>Footer</footer>
+        <Footer/>
       </body>
     </html>
   )
