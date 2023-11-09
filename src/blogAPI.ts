669fa8d91fdf447c209d1bf38791bbc8f9fb08c6
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-09 11:49:44 +0900

    記事削除APIを作成

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -49,3 +49,12 @@ export const createArticle = async (
   const newArticle = await res.json();
   return newArticle;
 };
+
+export const deleteArticle = async (id: string,): Promise<Article> => {
+  const res = await fetch(`http://localhost:3002/posts/${id}`, { method: "DELETE" });
+  if (!res.ok) {
+    throw new Error("記事を削除できませんでした。");
+  }
+  const deleteArticle = await res.json();
+  return deleteArticle;
+};
