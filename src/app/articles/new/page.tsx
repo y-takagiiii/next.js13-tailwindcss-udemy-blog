557974227deec0c0b8eb2b43b7139b58e0f47b48
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-09 10:46:47 +0900

    ブログ新規作成ページでcreateArticle関数を呼び出すように修正

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -1,23 +1,64 @@
-import React from 'react'
+"use client"
+import { createArticle } from '@/blogAPI';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 const CreateBlogPage = () => {
+  const router = useRouter();
+  const [id, setId] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    await createArticle(id, title, content);
+    setLoading(false);
+    router.push("/");
+    // pushだけだと状態を保持したまま画面遷移を行い、データの再読み込みは行われない
+    // refreshでサーバーにリクエストを送り、データを再取得させる
+    router.refresh();
+  }
   return (
     <div className='min-h-screen py-8 px-4 md:px-12'>
       <h2 className='text-2xl font-bold mb-4'>ブログ新規作成</h2>
-      <form className=" bg-slate-200 p-6 rounded shadow-lg">
+      <form
+        className=" bg-slate-200 p-6 rounded shadow-lg"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4">
           <label htmlFor="" className="text-gray-700 text-sm font-bold mb-2">URL</label>
-          <input type="text" className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"/>
+          <input
+            type="text"
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setId(e.target.value)}
+          />
         </div>
         <div className="mb-4">
           <label htmlFor="" className="text-gray-700 text-sm font-bold mb-2">タイトル</label>
-          <input type="text" className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"/>
+          <input
+            type="text"
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="mb-4">
           <label htmlFor="" className="text-gray-700 text-sm font-bold mb-2">本文</label>
-          <textarea className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"/>
+          <textarea
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setContent(e.target.value)}
+          />
         </div>
-        <button type='submit' className=' py-2 px-4 border rounded-md bg-orange-300'>投稿</button>
+        <button
+          type='submit'
+          className={`py-2 px-4 border rounded-md ${loading
+            ? "bg-orange-300 cursor-not-allowed"
+            : "bg-orange-400 hover:bg-orange-500"}`
+          }
+          disabled={loading}
+        >
+            投稿
+        </button>
       </form>
     </div>
   )
