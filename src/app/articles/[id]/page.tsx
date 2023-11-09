cadb540a547eea859f4e886f7437249ddf68e72b
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-09 11:51:41 +0900

    DeleteButtonコンポーネントを読み込み

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DeleteButton from '@/app/components/DeleteButton';
 import { getDetailArticle } from '@/blogAPI';
 import Image from 'next/image';
 import React from 'react'
@@ -17,6 +18,10 @@ const Article = async ({ params }: { params: { id: string } }) => {
       </h1>
       <div className='text-lg leading-relaxed text-justify'>
         <p>{detailArticle.content}</p>
+      </div>
+      <div className='text-right mt-3'>
+        {/* 基本的にはSSRを使いたいので、記事ページ全体はSCとして扱い、削除ボタンだけCCにする */}
+        <DeleteButton id={detailArticle.id}/>
       </div>
     </div>
   )
